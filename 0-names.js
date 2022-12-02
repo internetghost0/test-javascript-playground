d8ce5d7898c47046f600376044c8d4ac79393839
zer0 <internetghost0@protonmail.com>
2022-12-02 14:26:19 +0000

    ready set go

---
+++ 0-names.js
@@ -0,0 +1,8 @@
+// local
+const secret = "S3CR3T_C0D3"
+
+// share
+const john = "John Cena"
+const peter = "Peter Pen"
+
+module.exports = { john, peter }
---
+++ 2-modules.js
@@ -0,0 +1,6 @@
+const { john, peter } = require("./0-names")
+const sayHi = require("./1-utils")
+
+sayHi("susan")
+sayHi(john)
+sayHi(peter)
