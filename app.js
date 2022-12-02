65977e1e83af8ba723f987b4788c9407dbd34a05
zer0 <internetghost0@protonmail.com>
2022-12-02 17:49:08 +0000

    fs async using util.promisify

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
-const { readFile } = require("fs")
-
-const getText = (path) => {
-    return new Promise((resolve, reject) => {
-        readFile(path, "utf8", (err, data) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(data)
-            }
-        })
-    })
-}
-
-// getText("./content/first.txt")
-//     .then((result) => console.log(result))
-//     .catch((err) => console.log(err))
+const { readFile, writeFile } = require("fs")
+const util = require("util")
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
 
 const start = async () => {
     try {
-        const first = await getText("./content/first.txt")
-        const second = await getText("./content/second.txt")
+        const first = await readFilePromise("./content/first.txt", "utf8")
+        const second = await readFilePromise("./content/second.txt", "utf8")
+        await writeFilePromise(
+            "./content/result-promises.txt",
+            `First time using promises, killme what thefuck is promisify ${first} ${second}`
+        )
         console.log(first, second)
     } catch (error) {
         console.log(error)
     }
 }
 start()
-setTimeout(() => {
-    console.log("ooooh")
-}, 100)
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, "utf8", (err, data) => {
+//             if (err) {
+//                 reject(err)
+//             } else {
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
+
+// getText("./content/first.txt")
+//     .then((result) => console.log(result))
+//     .catch((err) => console.log(err))
