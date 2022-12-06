b9d2407ca44d0b1b40c8d99f2652cc3a6ffaba54
zer0 <internetghost0@protonmail.com>
2022-12-06 16:30:06 +0000

    add comments to `4-path-module`

--- 4-path-module.js
+++ 4-path-module.js
@@ -1,12 +1,20 @@
 const path = require("path")
 
+// path.sep: separator of path of your OS
 console.log(path.sep)
+// output: '/'
 
+// path.join: concatanete a strings to build path
 const filePath = path.join("content", "subfolder", "test.txt")
 console.log(filePath)
+// output: 'content/subfolder/test.txt'
 
+// path.basename: basename of path
 const base = path.basename(filePath)
 console.log(base)
+// output: 'test.txt'
 
+// path.resolve: concatenate a strings and return absolute path
 const absolute = path.resolve(__dirname, "content", "subfolder", "test.txt")
 console.log(absolute)
+// output: '/home/user/coding/test-javascript-playground/content/subfolder/test.txt'
